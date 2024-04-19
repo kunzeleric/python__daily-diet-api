5dc1c442903757cad688503c845333ffe8ef2ef1
Eric Kunzel <kunzel.eric@gmail.com>
2024-04-19 08:49:09 -0300

    feat: separated user routes and implemented create user route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from database import db
 from models.user import User
+from routes.user.routes import user_routes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "S2@#!4S1WSA1SS#@#$12311#@!#!@"
@@ -9,34 +10,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin123@127.0.0.1
 
 # DB initialization
 db.init_app(app)
+
 # Login config
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+# Register user route blueprint
+app.register_blueprint(user_routes)
+
 # Callback function to reload user object from user ID stored in session
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
-
-# Login Route
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.json
-    if data.username and data.password:
-        user = User.query.filter_by(username=data.username).first()
-        if user and user.check_password(data.password):
-            login_user(user)
-            return jsonify({ "message": "Logged in successfully!" }), 200
-        else:
-            return jsonify({ "message": "Credentials are invalid!" }), 400
-
-# Logout Route
-@app.route("/logout", methods=["GET"])
-@login_required
-def logout():
-    logout_user()
-    return jsonify({ "message": "Logged out successfully!" }), 200
+    return User.query.get(user_id)
 
 @app.route("/", methods=["GET"])
 def health_status():
---
+++ routes/user/routes.py
@@ -0,0 +1,41 @@
+from flask import request, jsonify, Blueprint
+from models.user import User
+from database import db
+from flask_login import login_user, logout_user, login_required, current_user
+
+user_routes = Blueprint('user_routes', __name__, static_folder='static', template_folder='templates')
+
+# Create a new user route
+@user_routes.route("/register", methods=["POST"])
+def register_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if username and password:
+      user = User(username=username, password=password)
+      db.session.add(user)
+      db.session.commit()
+      return jsonify({ "message": "Registered successfully!" }), 200
+    
+    return jsonify({ "message": "Error creating user, please validate your data." }), 400
+
+# Login Route
+@user_routes.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return jsonify({ "message": "Logged in successfully!" }), 200
+        else:
+            return jsonify({ "message": "Credentials are invalid!" }), 400
+
+# Logout Route
+@user_routes.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({ "message": "Logged out successfully!" }), 200
